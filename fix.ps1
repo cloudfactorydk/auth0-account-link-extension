fdc773117fbb451ae8a6dc55d26a682b48cd7c33
Nils Anders Hvinden <nilsanders.hvinden@dfo.no>
2023-03-31 14:50:35 +0200

    Added fix of node_modules dependency.

--- fix.ps1
+++ fix.ps1
@@ -1,1 +1,2 @@
 ((Get-Content -path node_modules\@auth0\hapi\lib\defaults.js -Raw) -replace 'Os.tmpDir','Os.tmpdir') | Set-Content -Path node_modules\@auth0\hapi\lib\defaults.js
+((Get-Content -path node_modules\auth0-extensions-cli\lib\externals.js -Raw) -replace 'JSON.parse\(file\).dependencies','{ try {return JSON.parse(file).dependencies} catch (e){}}') | Set-Content -Path node_modules\auth0-extensions-cli\lib\externals.js
