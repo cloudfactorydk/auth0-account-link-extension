ed9d893e48633f564a8a2d9b52f05a2ca922860c
Nils Anders Hvinden <nilsanders.hvinden@dfo.no>
2023-03-31 15:05:15 +0200

    CloudFactory CSS

--- templates/utils/defaultTemplate.js
+++ templates/utils/defaultTemplate.js
@@ -10,9 +10,37 @@ module.exports = `
     <link rel="shortcut icon" href="https://auth0.com/auth0-styleguide/img/favicon.png" />
     {{ ExtensionCSS }}
     {{ CustomCSS }}
+    <style type="text/css">
+      .auth0-lock-header-logo {
+            height: 35px !important;
+      }
+      .auth0-lock-content {
+            padding: 0 20px 20px !important;
+      }
+      #skip {
+            display: none;
+      }
+      #message {
+            display: none;
+      }
+      #content-container:after {
+            content: 'Continue below to sign in with your email and password, which will link your existing account to your Microsoft 365 account.';
+                              color: grey;
+                              white-space: pre-wrap;
+      }
+      #content-container {
+            text-align: center;
+                              line-height: 22px;
+            margin-bottom: 15px;
+            font-size: 17px;
+      }
+      form {
+            width: 400px !important;
+      }
+    </style>
   </head>
   <body>
-    
+
     {{ Auth0Widget }}
     {{ ExtensionScripts }}
   </body>
